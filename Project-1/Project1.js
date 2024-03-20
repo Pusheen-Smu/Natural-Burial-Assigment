2dadf666dc1e706ef137306f62936d863282542f
Ziyang (Saint Mary's University Student) <138800717+Pusheen-Smu@users.noreply.github.com>
2024-03-20 17:40:18 -0300

    Add files via upload

---
+++ Project-1/Project1.js
@@ -0,0 +1,71 @@
+// Audio Functions
+function darkModeHelper() {
+    let darkModeHelperAudio = new Audio('darkModeHelper.mp3');
+    darkModeHelperAudio.play();
+}
+
+function nameHelper() {
+    let nameHelperAudio = new Audio('nameHelper.mp3');
+    nameHelperAudio.play();
+}
+
+function dateBirthHelper() {
+    let dateBirthHelperAudio = new Audio('dateBirthHelper.mp3');
+    dateBirthHelperAudio.play();
+}
+
+function dateDiesHelper() {
+    let dateDiesHelperAudio = new Audio('dateDiesHelper.mp3');
+    dateDiesHelperAudio.play();
+}
+
+function phoneHelper() {
+    let phoneHelperAudio = new Audio('phoneHelper.mp3');
+    phoneHelperAudio.play();
+}
+
+function addressHelper() {
+    let addressHelperAudio = new Audio('addressHelper.mp3');
+    addressHelperAudio.play();
+}
+
+function burialAddressHelper() {
+    let burialAddressHelperAudio = new Audio('burialAddressHelper.mp3');
+    burialAddressHelperAudio.play();
+}
+
+function realtionHelper() {
+    let realtionHelperAudio = new Audio('realtionHelper.mp3');
+    realtionHelperAudio.play();
+}
+
+function burialTypeHelper() {
+    let burialTypeHelperAudio = new Audio('burialTypeHelper.mp3');
+    burialTypeHelperAudio.play();
+}
+
+function burialMethodHelper() {
+    let burialMethodHelperAudio = new Audio('burialMethodHelper.mp3');
+    burialMethodHelperAudio.play();
+}
+
+function markerHelper() {
+    let markerHelperAudio = new Audio('markerHelper.mp3');
+    markerHelperAudio.play();
+}
+
+function personalMsgHelper() {
+    let personalMsgHelperAudio = new Audio('personalMsgHelper.mp3');
+    personalMsgHelperAudio.play();
+}
+
+function initialHelper() {
+    let initialHelperAudio = new Audio('initialHelper.mp3');
+    initialHelperAudio.play();
+}
+
+
+
+
+
+//no content yet
