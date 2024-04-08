cb3ed4c8c978ce9e48ef97275822dfb0f8ac5610
abhii42821 <162752537+abhii42821@users.noreply.github.com>
2024-04-08 20:49:24 -0300

    Update server.js

--- Project 3/server.js
+++ Project 3/server.js
@@ -1,19 +1,4 @@
-/* The program contains functions for handling user input, storing
-data in local storage.
-Author(Group): 
-        Abhishek Abhishek-(A00480219) (Group Leader)
-        Madhur Sharma-(A00477319)
-        Mariam Nasir-(A00460192)
-        Preksha Joon-(A00469476)
-        Ziyang Xia-(A00475502)
-*/
-// Function to get user input data
-// how to use
-// 1. put it to your server
-// 2. node server.js
-// 3. you can use client
-// 4. kill terminal and port on server as server port wont refresh
-// 5. restart server.js
+let storageJSON = {};
 let express = require('express');
 let app = express();
 let port = 1488;
@@ -22,54 +7,18 @@ let port = 1488;
 app.use(express.json());
 
 // This object will store the last saved JSON
-let savedJson = const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors'); // Import CORS package
-const app = express();
-const port = 1488;
-
-// Use CORS middleware to enable CORS
-app.use(cors());
-
-// Middleware to parse JSON bodies
-app.use(bodyParser.json());
-
-// In-memory storage for simplicity. added some value to prevent null
-let storedData = {
-    fullName: "Saint Mary's University",
-    birthDate: '01/01/1802',
-    contactPhone: '9024205400',
-    contactEmail: 'welcome@smu.ca',
-    contactAddress: '923 Robie Halifax',
-    burialAddress: '923 Robie Halifax',
-    relationDeceased: 'others',
-    burialCemeterie: 'hybrid_grounds',
-    burialMethod: 'treePod',
-    burialMarker: 'wooden Plank',
-    message: "Our Campus is based on Mi'kma'ki, the ancestral and uncoded territory of the Mi'kmaq",
-    initials: 'SMU'
-  };
-
-app.post('/submit', (req, res) => {
-    // Store the incoming JSON in memory
-    storedData = req.body;
-    console.log('Data received:', storedData);
-    // Respond to the client to acknowledge receipt
-    res.send('Data has been received and stored.');
-});
-
-app.get('/download', (req, res) => {
-    // Send the stored JSON back to the client
-    if (Object.keys(storedData).length) {
-        res.json(storedData);
-    } else {
-        res.status(404).send('No data found');
-    }
-});
-
-app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`);
-});;
+let savedJson = { fullName: "John Doe",
+birthDate: "",
+contactPhone: "",
+contactEmail: "",
+contactAddress: "",
+burialAddress: "",
+relationDeceased: "family", // Defaulting to the first option
+burialCemeterie: "Hybrid grounds", // Defaulting to the first option
+burialMethod: "Shrouds", // Defaulting to the first option
+burialMarker: "", // You can set a default value here if needed
+message: "",
+initials: ""};
 
 app.post('/save', (req, res) => {
   // Save the JSON from the request body
